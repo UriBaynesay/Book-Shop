634a995946712d080c0bc4c5fb257fe3663bc92a
Uri Baynesay <uri552@gmail.com>
2022-04-05 16:02:56 +0300

    added the support for hebrew in static html elemnts

---
+++ js/books.controller.js
@@ -0,0 +1,73 @@
+"use strict";
+
+function init() {
+  renderBooks();
+  doTrans();
+}
+
+function renderBooks() {
+  const books = getBooksForDisplay();
+  var strHTML;
+  strHTML = books.map(function (book) {
+    return `<tr>
+        <td>${book.id}</td>
+        <td>${book.title}</td>
+        <td>${book.price}</td>
+        <td><button class="btn-review" onclick="onReview('${book.id}')">Review</button></td>
+        <td><button class="btn-update" onclick="onUpdateBook('${book.id}')">Update</button></td>
+        <td><button class="btn-delete" onclick="onDeleteBook('${book.id}')">Delete</button></td>
+        </tr>`;
+  });
+
+  const elTableBody = document.querySelector("tbody");
+  elTableBody.innerHTML = strHTML.join("");
+}
+
+function onAddRate(bookId) {
+    updateBookRate(bookId,'+');
+    onReview(bookId);
+}
+
+function onSubRate(bookId) {
+    updateBookRate(bookId,'-');
+    onReview(bookId);
+}
+
+function onReview(bookId) {
+  const bookDesc = getBookDesc(bookId);
+  const bookName = `<h2>${getBookName(bookId)}</h2>`;
+  const elModal = document.querySelector(".modal-container");
+  elModal.innerHTML =
+    bookName +
+    bookDesc +
+    `<button class="btn-ext-modal" onclick="onCloseModal()">X</button>
+    <div class="rate">
+      <button class="add-rate" onclick="onAddRate('${bookId}','+')">+</button>
+      <a class="book-rate">${getBookRate(bookId)}</a>
+      <button class="sub-rate" onclick="onSubRate('${bookId}','-')">-</button>
+    </div>`;
+  elModal.classList.add("show");
+}
+
+function onCloseModal(){
+    const elModal = document.querySelector(".modal-container");
+    elModal.classList.remove("show");
+}
+
+function onUpdateBook(bookId) {
+  const newPrice = +prompt("Please enter the new price");
+  UpdateBook(bookId, newPrice);
+  renderBooks();
+}
+
+function onDeleteBook(bookId) {
+  removeBook(bookId);
+  renderBooks();
+}
+
+function onAddBook() {
+  const bookName = prompt("Please enter the books title");
+  const bookPrice = prompt("please enter the books price");
+  AddBook(bookName, bookPrice);
+  renderBooks();
+}
---
+++ services/i18n.service.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const G_TRANS={
+    greet:{
+        en:'Welcome Back!',
+        he:"ברוכים השבים!"
+    },
+    id:{
+        en:'ID',
+        he:"קוד"
+    },
+    title:{
+        en:'Title',
+        he:"כותרת"
+    },
+    price:{
+        en:'Price',
+        he:'מחיר'
+    },
+    actions:{
+        en:'Actions',
+        he:'פעולות'
+    },
+    add:{
+        en:'Add Book',
+        he:'הוסף ספר'
+    }
+};
+
+var gCurrLan='he'
+
+function getTrans(transKey){
+    const transOptions=G_TRANS[transKey];
+    if(!transOptions) return;
+    const currLanTrans=transOptions[gCurrLan];
+    if(!currLanTrans) return;
+    return currLanTrans;
+}
+
+function doTrans(){
+    debugger
+    const elDoc=document.body;
+    elDoc.classList.add('rtl')
+    const elTxts=elDoc.querySelectorAll('[data-trans]');
+    elTxts.forEach((txt) => txt.innerText=getTrans(txt.dataset.trans));
+}
